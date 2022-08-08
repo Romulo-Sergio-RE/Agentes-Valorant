f8b4795ea6ac05e9c97ce6a369a5796906ac1a44
Romulo-Sergio-RE <89258510+Romulo-Sergio-RE@users.noreply.github.com>
2022-08-08 18:14:25 -0300

    page e titulo do projeto
    tarminando de criar as pastas e mudando o nome e o icone do projeto

---
+++ src/app/pages/favoritosPage/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./favoritosStyle";
+
+export const AgentesFavoritos = ()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
--- src/app/pages/homePage/index.tsx
+++ src/app/pages/homePage/index.tsx
@@ -1,3 +1,5 @@
+import { Container } from "./homePageStyle"
+
 export const HomePage=()=>{
     return(
         <div>
